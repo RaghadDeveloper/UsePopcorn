743efb656f3b89361a7a1a22f71c46de48c83cc6
RaghadDeveloper <raghadmh620@gmail.com>
2025-03-20 17:17:46 +0300

    StarRating

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import StarRating from "./Components/StarRating.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    {/* <StarRating
+      // maxRating={7}
+      messages={["tribble", "bad", "okay", "good", "amazing"]}
+    />
+    <StarRating maxRating={7} /> */}
+  </StrictMode>
+);
